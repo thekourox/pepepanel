95f9cacb76c71b685314c84e7bee02e40869240d
Kourosh <thekourox@gmail.com>
2026-07-08 10:20:40 -0700

    security tor and shark fixed

--- pasarguard-tor/api.py
+++ pasarguard-tor/api.py
@@ -396,4 +396,4 @@ async def handle_lifecycle(req: LifecycleRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=54322)
+    uvicorn.run(app, host="127.0.0.1", port=54322)
